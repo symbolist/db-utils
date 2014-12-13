3caf86803ba63707c3a344c8f93ead26be1ec193
Usman Khalid <2200617@gmail.com>
2014-12-13 17:38:29 +0500

    Added tests for nested combinations of isolation level changes.

--- db_utils/tests/test_transaction.py
+++ db_utils/tests/test_transaction.py
@@ -85,6 +85,126 @@ class TransactionDecoratorsTestCase(TransactionTestCase):
         self.assertIsNone(thread2.status.get('exception'))
         self.assertEqual(thread2.status.get('created'), created_in_2)
 
+
+    @ddt.data(
+        (
+            read_committed_transactions, read_committed_transactions, read_committed_transactions,
+            [u'student_0@edx.org', u'student_2@edx.org', u'student_3@edx.org', u'student_4@edx.org', u'student_5@edx.org']
+        ),
+        (
+            repeatable_read_transactions, repeatable_read_transactions, read_committed_transactions,
+            [None, u'student_1@edx.org', u'student_1@edx.org', u'student_4@edx.org', u'student_5@edx.org']
+        ),
+        (
+            read_committed_transactions, repeatable_read_transactions, read_committed_transactions,
+            [u'student_0@edx.org', u'student_1@edx.org', u'student_1@edx.org', u'student_4@edx.org', u'student_5@edx.org']
+        ),
+        (
+            repeatable_read_transactions, read_committed_transactions, read_committed_transactions,
+            [None, u'student_2@edx.org', u'student_3@edx.org', u'student_4@edx.org', u'student_5@edx.org']
+        ),
+        (
+            read_committed_transactions, read_committed_transactions, repeatable_read_transactions,
+            [u'student_0@edx.org', u'student_2@edx.org', u'student_3@edx.org', u'student_3@edx.org', u'student_3@edx.org']
+        ),
+        (
+            repeatable_read_transactions, repeatable_read_transactions, repeatable_read_transactions,
+            [None, u'student_1@edx.org', u'student_1@edx.org', u'student_3@edx.org', u'student_3@edx.org']
+        ),
+        (
+            read_committed_transactions, repeatable_read_transactions, repeatable_read_transactions,
+            [u'student_0@edx.org', u'student_1@edx.org', u'student_1@edx.org', u'student_3@edx.org', u'student_3@edx.org']
+        ),
+        (
+            repeatable_read_transactions, read_committed_transactions, repeatable_read_transactions,
+            [None, u'student_2@edx.org', u'student_3@edx.org', u'student_3@edx.org', u'student_3@edx.org']
+        ),
+    )
+    @ddt.unpack
+    def test_concurrent_requests_2(self, transaction_manager_series_1, transaction_manager_series_2, transaction_manager_series_3, results):
+        """
+        Test combinations of nested isolation level changes.
+        """
+
+        if connection.vendor != 'mysql':
+            raise unittest.SkipTest('Only works on MySQL.')
+
+
+        class RequestThread1(threading.Thread):
+            """A thread which runs a dummy view."""
+            def __init__(self, delay, **kwargs):
+                super(RequestThread1, self).__init__(**kwargs)
+                self.delay = delay
+
+            def run(self):
+                """A dummy view."""
+
+                time.sleep(self.delay)
+                User.objects.create(username='student', email='student_0@edx.org')
+
+                for index in xrange(1, 8):
+                    time.sleep(self.delay)
+                    with commit_on_success():
+                        u = User.objects.get(username='student')
+                        u.email = 'student_{}@edx.org'.format(index)
+                        u.save()
+
+
+        class RequestThread2(threading.Thread):
+            """A thread which runs a dummy view."""
+            def __init__(self, delay, **kwargs):
+                super(RequestThread2, self).__init__(**kwargs)
+                self.delay = delay
+                self.results = []
+
+            @commit_on_success()
+            def run(self):
+                """A dummy view."""
+
+                for transaction_manager_1 in transaction_manager_series_1(delay=0, max_attempts=2):
+                    with transaction_manager_1:
+                        User.objects.filter(username='student').exists()  # To start the transaction
+
+                        time.sleep(self.delay)
+                        #self.results.append(User.objects.get_or_create(username='student'))
+                        if User.objects.filter(username='student').exists():
+                            self.results.append(User.objects.get(username='student').email)
+                        else:
+                            self.results.append(None)
+
+                        time.sleep(self.delay)
+                        for transaction_manager_2 in transaction_manager_series_2(delay=0, max_attempts=2):
+                            with transaction_manager_2:
+                                User.objects.filter(username='student').exists()  # To start the transaction
+
+                                time.sleep(self.delay)
+                                self.results.append(User.objects.get(username='student').email)
+
+                                time.sleep(self.delay)
+                                self.results.append(User.objects.get(username='student').email)
+
+                        for transaction_manager_3 in transaction_manager_series_3(delay=0, max_attempts=2):
+                            with transaction_manager_3:
+                                User.objects.filter(username='student').exists()  # To start the transaction
+
+                                time.sleep(self.delay)
+                                self.results.append(User.objects.get(username='student').email)
+
+                                time.sleep(self.delay)
+                                self.results.append(User.objects.get(username='student').email)
+
+
+        thread1 = RequestThread1(delay=1)
+        thread2 = RequestThread2(delay=1)
+
+        thread1.start()
+        time.sleep(0.5)
+        thread2.start()
+        thread2.join()
+        thread1.join()
+
+        self.assertEqual(thread2.results, results)
+
     @ddt.data(
         (commit_on_success_with_read_committed,),
         (commit_on_success_with_repeatable_read,),
